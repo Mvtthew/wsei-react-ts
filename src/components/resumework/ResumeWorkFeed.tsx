6737b5bc076e1f12ffece5896c03a0b3bf4a203d
Mateusz Ożóg <mateusz2345678@gmail.com>
2021-06-26 13:10:54 +0200

    pagination

--- src/components/resumework/ResumeWorkFeed.tsx
+++ src/components/resumework/ResumeWorkFeed.tsx
@@ -1,8 +1,21 @@
+import { useSelector, useStore } from 'react-redux';
+
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { Post } from '../../store/postsReducer';
 import React from 'react';
+import { RootState } from '../../store';
+import { User } from '../../store/usersReducer';
+import moment from 'moment';
+import { useState } from 'react';
 
 const ResumeWorkFeed: FC = () => {
+	const posts = useSelector<RootState, Post[]>((store) => store.posts);
+	const users = useSelector<RootState, User[]>((store) => store.users.users);
+	const [page, setPage] = useState(1);
+
+	const pageSize = 10;
+
 	return (
 		<div>
 			<h1>Resume your work</h1>
@@ -11,28 +24,47 @@ const ResumeWorkFeed: FC = () => {
 				<i className='bx bx-right-arrow-alt'></i>
 			</Link>
 			<div className='resume-work-feed mt-4'>
-				<div className='resume-work-feed-item'>
-					<h4>Title</h4>
-					<p>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat, sapiente quia fugiat ducimus veritatis suscipit velit
-						doloremque illum enim magnam quae quis cumque incidunt magni vero aliquam beatae totam natus.
-					</p>
-					<div className='d-flex'>
-						<div>
-							<i className='bx bx-accessibility'></i>
-							Company
+				{posts.map((p, i) =>
+					i < page * pageSize && i > (page - 1) * pageSize ? (
+						<div className='resume-work-feed-item mb-3'>
+							<h4>{p.title}</h4>
+							<p>{p.body}</p>
+							<div className='d-flex'>
+								<div>
+									<i className='bx bx-accessibility'></i>
+									{users[p.id % 10].name}
+								</div>
+								<div className='ms-3'>
+									<i className='bx bx-building'></i>
+									{users[p.id % 10].company.name}
+								</div>
+								<div className='ms-3'>
+									<i className='bx bx-calendar'></i>
+									Updated{' '}
+									{moment()
+										.subtract(Math.ceil(Math.random() * 30), 'day')
+										.fromNow()}
+								</div>
+							</div>
 						</div>
-						<div>
-							<i className='bx bx-building'></i>
-							Corporate
-						</div>
-						<div>
-							<i className='bx bx-calendar'></i>
-							Updated 5 days ago
-						</div>
-					</div>
-				</div>
+					) : (
+						<></>
+					),
+				)}
 			</div>
+			<nav aria-label='Page navigation example'>
+				<ul className='pagination'>
+					{posts.map((p, i) =>
+						i * pageSize <= posts.length ? (
+							<li className={'page-item ' + (i + 1 === page ? 'active' : '')}>
+								<a className='page-link' onClick={() => setPage(i + 1)}>
+									{i + 1}
+								</a>
+							</li>
+						) : null,
+					)}
+				</ul>
+			</nav>
 		</div>
 	);
 };
--- src/components/workspaces/WorkspacesTiles.tsx
+++ src/components/workspaces/WorkspacesTiles.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { Photo } from '../../store/photosReducer';
+import { Post } from '../../store/postsReducer';
 import React from 'react';
+import { RootState } from '../../store';
+import { useSelector } from 'react-redux';
 
 const WorkspacesTiles: FC = () => {
+	const posts = useSelector<RootState, Post[]>((store) => store.posts);
+	const photos = useSelector<RootState, Photo[]>((state) => state.photos);
+
 	return (
 		<div>
 			<h1>Workspaces</h1>
@@ -11,120 +18,31 @@ const WorkspacesTiles: FC = () => {
 				<i className='bx bx-right-arrow-alt'></i>
 			</Link>
 			<div className='workspaces-tiles mt-4'>
-				<div className='workspaces-tile'>
-					<div className='header-img'></div>
-					<div className='header'>
-						<div className='icon-box'>
-							<i className='bx bx-file'></i>
+				{posts.map((p, i) =>
+					i > 5 && i < 10 ? (
+						<div className='workspaces-tile'>
+							<div className='header-img' style={{ backgroundImage: `url(${photos[p.id].url})` }}></div>
+							<div className='header'>
+								<div className='icon-box'>
+									<i className='bx bx-file'></i>
+								</div>
+								<h4>{p.title.substr(0, 12)}</h4>
+							</div>
+							<div className='d-flex px-4'>
+								<p className='me-4'>
+									<i className='bx bx-brain'></i>
+									{p.body.split(' ')[0].substr(0, 9)}
+								</p>
+								<p>
+									<i className='bx bx-user'></i>
+									{Math.ceil(Math.random() * 500)} users
+								</p>
+							</div>
 						</div>
-						<h4>Client contract</h4>
-					</div>
-					<div className='d-flex px-4'>
-						<p className='me-4'>
-							<i className='bx bx-brain'></i>
-							Contract
-						</p>
-						<p>
-							<i className='bx bx-user'></i>
-							150 users
-						</p>
-					</div>
-				</div>
-				<div className='workspaces-tile'>
-					<div className='header-img'></div>
-					<div className='header'>
-						<div className='icon-box'>
-							<i className='bx bx-file'></i>
-						</div>
-						<h4>Client contract</h4>
-					</div>
-					<div className='d-flex px-4'>
-						<p className='me-4'>
-							<i className='bx bx-brain'></i>
-							Contract
-						</p>
-						<p>
-							<i className='bx bx-user'></i>
-							150 users
-						</p>
-					</div>
-				</div>
-				<div className='workspaces-tile'>
-					<div className='header-img'></div>
-					<div className='header'>
-						<div className='icon-box'>
-							<i className='bx bx-file'></i>
-						</div>
-						<h4>Client contract</h4>
-					</div>
-					<div className='d-flex px-4'>
-						<p className='me-4'>
-							<i className='bx bx-brain'></i>
-							Contract
-						</p>
-						<p>
-							<i className='bx bx-user'></i>
-							150 users
-						</p>
-					</div>
-				</div>
-				<div className='workspaces-tile'>
-					<div className='header-img'></div>
-					<div className='header'>
-						<div className='icon-box'>
-							<i className='bx bx-file'></i>
-						</div>
-						<h4>Client contract</h4>
-					</div>
-					<div className='d-flex px-4'>
-						<p className='me-4'>
-							<i className='bx bx-brain'></i>
-							Contract
-						</p>
-						<p>
-							<i className='bx bx-user'></i>
-							150 users
-						</p>
-					</div>
-				</div>
-				<div className='workspaces-tile'>
-					<div className='header-img'></div>
-					<div className='header'>
-						<div className='icon-box'>
-							<i className='bx bx-file'></i>
-						</div>
-						<h4>Client contract</h4>
-					</div>
-					<div className='d-flex px-4'>
-						<p className='me-4'>
-							<i className='bx bx-brain'></i>
-							Contract
-						</p>
-						<p>
-							<i className='bx bx-user'></i>
-							150 users
-						</p>
-					</div>
-				</div>
-				<div className='workspaces-tile'>
-					<div className='header-img'></div>
-					<div className='header'>
-						<div className='icon-box'>
-							<i className='bx bx-file'></i>
-						</div>
-						<h4>Client contract</h4>
-					</div>
-					<div className='d-flex px-4'>
-						<p className='me-4'>
-							<i className='bx bx-brain'></i>
-							Contract
-						</p>
-						<p>
-							<i className='bx bx-user'></i>
-							150 users
-						</p>
-					</div>
-				</div>
+					) : (
+						<></>
+					),
+				)}
 			</div>
 		</div>
 	);
